9bd07f0a38d465e3b726b186774b1c9e2f2210db
Andres <jorge.hez2005@gmail.com>
2021-08-02 18:37:02 -0500

    Testing getting product data/image from the database

--- src/main/java/andres/learning/CRUD/authentication/controller/Controller.java
+++ src/main/java/andres/learning/CRUD/authentication/controller/Controller.java
@@ -1,11 +1,12 @@
 package andres.learning.CRUD.authentication.controller;
 
-import javax.annotation.Resource;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
-import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
 
 @WebServlet(name = "Controller", value = "/Controller")
 public class Controller extends HttpServlet {
@@ -16,6 +17,39 @@ public class Controller extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter output = response.getWriter();
+        output.println("<html><body style='text-align: center;'");
+
+        int pictureId = Integer.parseInt(request.getParameter("product"));
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jsproject",
+                    "root", "mysqlandres");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM products WHERE id=?;");
+            preparedStatement.setInt(1, pictureId);
+            ResultSet result=preparedStatement.executeQuery();
+            if(result.next()){
+                output.println("<h1>PRODUCT</h1>");
+                String productName = result.getString("productName");
+                String description = result.getString("description");
+                String price = result.getString("price");
+                String picturePath = result.getString("fileName");
+
+                output.println("<h3>"+productName+"</h3>");
+                output.println("<image src='pictures/"+picturePath+"' width=150 />");
+                output.println("<p>DESCRIPTION: "+description+"</p>");
+                output.println("<p>PRICE: "+price+"</p>");
+            }
+            else{
+                output.println("<h3>Picture not found</h3>");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        output.println("</body></html>");
 
     }
 
@@ -23,6 +57,7 @@ public class Controller extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
+
     public boolean checkValidUser(HttpServletRequest request, HttpServletResponse response) {
         Cookie[] requestCookies = request.getCookies();
         boolean validUser = false;
